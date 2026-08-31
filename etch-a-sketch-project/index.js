ba31e6b76af6850354b047079f160c5e286efdd6
Rabit Ebibi <rabit2245@gmail.com>
2026-08-31 20:39:48 +0200

    added hover effect on mouseover the grids

--- etch-a-sketch-project/index.js
+++ etch-a-sketch-project/index.js
@@ -1,9 +1,13 @@
 function createGrid(size) {
   const container = document.querySelector('.container');
-  
+
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
+
+    square.addEventListener('mouseover', () => {
+      square.classList.add('hovered');
+    });
     container.appendChild(square);
   }
 }
